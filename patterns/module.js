3849f7e26f29ac4ab15b317b626795d006527799
Pavel <pvllnspk@gmail.com>
2016-01-17 13:05:45 +0300

    Dev

---
+++ patterns/module.js
@@ -0,0 +1,23 @@
+var Module = (function () {
+
+    var _privateMethod = function () {
+        console.log("Hello 1");
+    };
+
+    var someMethod = function () {
+        _privateMethod();
+    };
+
+    var anotherMethod = function (text) {
+        console.log(text);
+    };
+
+    return {
+        someMethod: someMethod,
+        anotherMethod: anotherMethod
+    };
+
+})();
+
+Module.someMethod();
+Module.anotherMethod('Hello 2');
